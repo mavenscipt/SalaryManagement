5987e4e09cbe51ff3b5bc684a6534a15cf9dc4bb
developerhub2444 <developerhub2444@gmail.com>
2021-03-03 00:13:20 +0530

    Init

---
+++ SalaryManagement/SalaryManagement/Operations.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+namespace SalaryManagement
+{
+   
+    class Operations
+    {
+        
+        public SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\Maindb.mdf;Integrated Security=True;User Instance=True;MultipleActiveResultSets=True");
+        public SqlConnection getConnection()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+        public int CreateUsers(string users,string Pass,int Role)
+        {
+                SqlCommand cmd = new SqlCommand("SPCreateUser");
+                cmd.Parameters.AddWithValue("Username", users);
+                cmd.Parameters.AddWithValue("Password", Pass);
+                cmd.Parameters.AddWithValue("Role", Role);
+                cmd.Connection = getConnection();
+                cmd.CommandType = CommandType.StoredProcedure;
+                return cmd.ExecuteNonQuery();        
+        }
+        public int UserExisting(string users)
+        {
+            SqlCommand cmd = new SqlCommand("SPGetUserCount");
+            cmd.Parameters.AddWithValue("Username", users);
+            SqlParameter Parm = new SqlParameter();
+            var returnparameters = cmd.Parameters.Add("count",SqlDbType.Int);
+            returnparameters.Direction = ParameterDirection.ReturnValue;
+            
+            cmd.Connection = getConnection();
+            cmd.CommandType = CommandType.StoredProcedure;
+             cmd.ExecuteNonQuery();        
+            return (int)returnparameters.Value;
+        }
+        public Dictionary<int,string> GetUserName()
+        {
+            Dictionary<int,string> Users = new Dictionary<int,string>();
+            Users.Add(0, "Select");
+            SqlCommand cmd = new SqlCommand("SPSelectAllUsers");
+            cmd.Connection = getConnection();
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Users.Add(Convert.ToInt32(dr[0].ToString()), dr[1].ToString());        
+            }
+            return Users;
+        }
+        public int ChangePassword(string User,string Password)
+        {
+            SqlCommand cmd = new SqlCommand("SPUpdatePassword");
+            cmd.Parameters.AddWithValue("Username", User);
+            cmd.Parameters.AddWithValue("Password", Password);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = getConnection();
+            return cmd.ExecuteNonQuery();         
+        }
+        public int DeleteUsers(string username)
+        {
+            SqlCommand cmd = new SqlCommand("SPDeleteuser");
+            cmd.Connection = getConnection();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("Username",username);
+            return cmd.ExecuteNonQuery();
+        }
+                     
+      
+    }
+}
---
+++ SalaryManagement/SalaryManagement/frmCreate.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SalaryManagement
+{
+    partial class frmCreate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(85, 225);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UserName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Role";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(125, 109);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUsername_KeyPress);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(125, 135);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // cmbRole
+            // 
+            this.cmbRole.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbRole.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.cmbRole.Location = new System.Drawing.Point(125, 166);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(100, 21);
+            this.cmbRole.TabIndex = 2;
+            this.cmbRole.Tag = "";
+            this.cmbRole.Text = "Role";
+            this.cmbRole.SelectedIndexChanged += new System.EventHandler(this.cmbRole_SelectedIndexChanged);
+            this.cmbRole.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cmbRole_KeyPress);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(180, 225);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(122, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Master Create Users";
+            // 
+            // Create
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 350);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.cmbRole);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Create";
+            this.Text = "Create";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ComboBox cmbRole;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
